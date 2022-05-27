2a6eeea4c6014e1bd97bb2a7f4fc77729b82ef32
alexander.boxhorn <alexander.boxhorn@muenchen.de>
2022-05-27 14:46:40 +0200

    add base packages

--- digiwf-okewo-integration-service/src/main/java/io/muenchendigital/digiwf/okewo/integration/api/OkEwoIntegrationApplication.java
+++ digiwf-okewo-integration-service/src/main/java/io/muenchendigital/digiwf/okewo/integration/api/OkEwoIntegrationApplication.java
@@ -4,7 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
-@SpringBootApplication
+@SpringBootApplication(
+        scanBasePackages = {
+                "io.muenchendigital.digiwf.okewo.integration",
+                "io.muenchendigital.digiwf.spring.cloudstream.utils"
+        }
+)
 public class OkEwoIntegrationApplication {
 
     public static void main(final String[] args) {
